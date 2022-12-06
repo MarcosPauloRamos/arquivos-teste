0c093096b83db901bb5e77e833ee28e108ce1169
MarcosPauloRamos <49656714+MarcosPauloRamos@users.noreply.github.com>
2022-12-06 13:15:49 -0300

    Delete source directory

--- source/parse.h
+++
@@ -1,9 +0,0 @@
-#ifndef _PARSE_H_
-#define _PARSE_H_
-
-/**
- * @brief Função parse retorna a árvore sintática recém construida
- */
-TreeNode * parse(void);
-
-#endif
--- source/run.sh
+++
@@ -1,5 +0,0 @@
-
-flex Scanner.l
-bison -d Parser.y
-gcc -c *.c -fno-builtin-exp -Wno-implicit-function-declaration
-gcc *.o -lfl -o compilador -fno-builtin-exp
